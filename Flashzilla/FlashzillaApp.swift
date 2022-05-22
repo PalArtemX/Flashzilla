5f7ac3fb4c3de6f112107da7f57aacf3974a4047
Artem Paliutin <pal.artem@icloud.com>
2022-05-22 13:45:00 +0300

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Artem Paliutin on 22/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
